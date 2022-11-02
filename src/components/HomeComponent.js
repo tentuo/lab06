ad8bbb452b83b4ac510f8bfc790ee4bcf1920dd6
tutmfx15408@funix.edu.vn <tutmfx15408@funix.edu.vn>
2022-11-02 21:47:05 +0700

    bai so lab6

---
+++ src/components/HomeComponent.js
@@ -0,0 +1,77 @@
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, CardImgOverlay} from 'reactstrap';
+import React, { Component } from "react";
+
+    
+class Home extends Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                selectedDish: null
+            }
+        }
+    
+        onDishSelect(dish) {
+            this.setState({ selectedDish: dish});
+        }
+
+    render() {
+        const menu1 = this.props.dish.map((dish) => {
+            return (
+                <Card>
+                <CardImg src={dish.image} alt={dish.name} />
+                <CardBody>
+                <CardTitle>{dish.name}</CardTitle>
+                {dish.designation ? <CardSubtitle>{dish.designation}</CardSubtitle> : null }
+                <CardText>{dish.description}</CardText>
+                </CardBody>
+                </Card>
+            );
+        });
+
+        const menu2 = this.props.promotion.map((promotion) => {
+            return (
+                <Card>
+                <CardImg src={promotion.image} alt={promotion.name} />
+                <CardBody>
+                <CardTitle>{promotion.name}</CardTitle>
+                {promotion.designation ? <CardSubtitle>{promotion.designation}</CardSubtitle> : null }
+                <CardText>{promotion.description}</CardText>
+                </CardBody>
+                </Card>
+            );
+        });
+
+        const menu3 = this.props.leader.map((leader) => {
+            return (
+                <Card>
+                <CardImg src={leader.image} alt={leader.name} />
+                <CardBody>
+                <CardTitle>{leader.name}</CardTitle>
+                {leader.designation ? <CardSubtitle>{leader.designation}</CardSubtitle> : null }
+                <CardText>{leader.description}</CardText>
+                </CardBody>
+                </Card>
+            );
+        });
+
+        return (
+            <div className="container" >
+                <div className="col-md-5 m-1" width="10%">
+                    {menu1}
+                </div>
+                <div className="container">
+                <div className="col-md-5 m-1" width="10%">
+                    {menu2}
+                </div>
+                </div>
+                <div className="container">
+                <div className="col-md-5 m-1" width="10%">
+                    {menu3}
+                </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
